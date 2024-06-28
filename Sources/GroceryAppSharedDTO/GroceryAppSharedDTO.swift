21ead555b2d133ed94a782cda91b25c739069808
Artem <114776102+amolon615@users.noreply.github.com>
2024-06-28 16:20:22 +0100

    Added GroceryCategoryRequestDTO & GroceryCategoryResponseDTO

--- Sources/GroceryAppSharedDTO/GroceryAppSharedDTO.swift
+++
@@ -1,8 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-public struct GroceryAppSharedDTO {
-    public private(set) var text = "Hello World!"
-    
-    public init() { }
-}
---
+++ Sources/GroceryAppSharedDTO/Models/GroceryCategoryRequestDTO.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by amolonus on 28/06/2024.
+//
+
+import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
